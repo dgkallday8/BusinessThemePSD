bdf2fc8254c1238dd9f242d2ea1ada357675ccf0
enotdgk <enotdgk@mail.com>
2020-09-02 15:10:17 +0300

    carousel

---
+++ script.js
@@ -0,0 +1,51 @@
+const slides = document.querySelectorAll('.slide')
+const points = document.querySelectorAll('.point')
+
+let index = 0
+
+const activeSlide = ind => {
+    for (let slide of slides) {
+        slide.classList.remove('active')
+    }
+    slides[ind].classList.add('active')
+}
+const activePoint = ind => {
+    for (let point of points) {
+        point.classList.remove('active')
+    }
+    points[ind].classList.add('active')
+}
+
+const nextSlide = () => {
+    if (index === slides.length - 1) {
+        index = 0
+        activeSlide(index)
+        activePoint(index)
+    } else {
+        index++
+        activePoint(index)
+        activeSlide(index)
+    }
+}
+
+const prevSlide = () => {
+    if (index === 0) {
+        index = slides.length - 1
+        activeSlide(index)
+        activePoint(index)
+    } else {
+        index--
+        activePoint(index)
+        activeSlide(index)
+    }
+}
+
+points.forEach((item, indexPoint) => {
+    item.addEventListener('click', () => {
+        index = indexPoint
+        activePoint(index)
+        activeSlide(index)
+        console.log(indexPoint)
+    })
+})
+
